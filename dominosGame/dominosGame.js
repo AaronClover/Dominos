cbab046ac99774ad203bfd5323087a0c971178f2
Aaron Clover <aaron.clover@gmail.com>
2013-12-12 20:14:37 -0800

    Added touch handler and animation loop

--- dominosGame/dominosGame.js
+++ dominosGame/dominosGame.js
@@ -8,6 +8,7 @@ var drawableArea = new pline(4);
 var testDomino = new domino(5,6,50);
 drawableArea.dependentDrawableItems.push(testDomino);
 
+
 var activeStatusBar = new playerStatusBar(1/10, 1/3);
 drawableArea.dependentDrawableItems.push(activeStatusBar);
 
@@ -17,8 +18,40 @@ drawableArea.dependentDrawableItems.push(passiveStatusBar);
 var boneYard = new boneyard(1/4, window.innerWidth, window.innerHeight);
 drawableArea.dependentDrawableItems.push(boneYard);
 
+
+var touchable = 'createTouch' in document;
+
+
+if(touchable) {
+	canvas.addEventListener( 'touchstart', onTouchStart, false );
+	canvas.addEventListener( 'touchmove', onTouchMove, false );
+	canvas.addEventListener( 'touchend', onTouchEnd, false );
+}
+/*************************************
+ * Animation loop
+ *
+ *
+ * requestAnim shim layer by Paul Irish Finds the first API that works to
+ * optimize the animation loop, otherwise defaults to setTimeout().
+ */
+window.requestAnimFrame = (function() {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame
+            || window.mozRequestAnimationFrame || window.oRequestAnimationFrame
+            || window.msRequestAnimationFrame
+            || function(/* function */callback, /* DOMElement */element) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
+
+
+function animate() {
+    requestAnimFrame( animate );
+    //update();
+    drawScreen();
+
+}
 //------------------------------------------
-// Draw loop
+// Draw
 //------------------------------------------
 function drawScreen() {
     var ctx = canvas.getContext('2d');
@@ -31,7 +64,7 @@ function drawScreen() {
             ctx.canvas.width, ctx.canvas.height,
             0, ctx.canvas.height);
     
-    testDomino.moveTo(ctx.canvas.width/3, ctx.canvas.height/3);
+    
     testDomino.changeSize(constrainedSize/10);
     
     //draw the active player's status bar on the bottom left
